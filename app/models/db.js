b283a50b6e8afee97224bd6002fd8fa51b1f60ce
Raymond Ton <33384164+pnoen@users.noreply.github.com>
2023-08-21 11:34:25 +1000

    Removed db link

--- app/models/db.js
+++ app/models/db.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://lalemany:xZ8YtOEII8Hs0YLI@l05g05.un2muoq.mongodb.net/oldPhoneDeals', { useNewUrlParser: true }, function () {
+// Replace with mongodb link
+mongoose.connect('dbLink', { useNewUrlParser: true }, function () {
   console.log('mongodb connected')
 });
 
